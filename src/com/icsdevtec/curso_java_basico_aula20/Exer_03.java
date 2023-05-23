2d346d091a70ece09d98fb24c1e801b2fdefa15e
icsdevtec <icsdevtec@DESKTOP-U0729KU>
2023-05-22 22:37:01 -0300

    commit execercios compromissos

--- src/com/icsdevtec/curso_java_basico_aula20/Exer_03.java
+++ src/com/icsdevtec/curso_java_basico_aula20/Exer_03.java
@@ -1,15 +1,46 @@
 package com.icsdevtec.curso_java_basico_aula20;
-/**
- * @author icsdevtec
- *
- */
+
+
+import java.util.Scanner;
+
+
 public class Exer_03 {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+    	
+     Scanner scan = new Scanner(System.in);
+        int[][] numerosMatriz = new int[3][3];
 
-	}
+       
+
+        // Preenchendo a matriz com valores do teclado
+        System.out.println("Digite os valores para preencher a matriz 3x3:");
+
+        for (int i = 0; i < numerosMatriz.length; i++) {
+            for (int j = 0; j < numerosMatriz[i].length; j++) {
+                System.out.print("Digite o valor para a posição [" + i + "][" + j + "]: ");
+                numerosMatriz[i][j] = scan.nextInt();
+            }
+        }
+
+        // Contando os números pares e ímpares
+        int countPares = 0;
+        int countImpares = 0;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (numerosMatriz[i][j] % 2 == 0) {
+                    countPares++;
+                } else {
+                    countImpares++;
+                }
+            }
+        }
+
+        System.out.println("Quantidade de números pares: " + countPares);
+        System.out.println("Quantidade de números ímpares: " + countImpares);
+    }
 
 }
--- src/com/icsdevtec/curso_java_basico_aula20/Exer_04.java
+++ src/com/icsdevtec/curso_java_basico_aula20/Exer_04.java
@@ -3,6 +3,8 @@
  */
 package com.icsdevtec.curso_java_basico_aula20;
 
+import java.util.Scanner;
+
 /**
  * @author icsdevtec
  *
@@ -13,8 +15,81 @@ public class Exer_04 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
 
+		String[][] compromissos = new String[31][24];
+
+		boolean sair = false;
+		boolean diaValido = false;
+
+		boolean hrValida = false;
+		int hora = 0;
+		int dia = 0;
+		byte opcao;
+		while (!sair) {
+			System.out.println(" Digite 1 para adicionar o compromisso !!! ");
+			System.out.println(" Digite 2 para verificar o compromisso !!! ");
+			System.out.println(" Digite 0 para sair do Programa !!! ");
+			opcao = scan.nextByte();
+
+			switch (opcao) {
+			// Adicionar o compromisso
+			case 1: {
+
+				while (!diaValido) {
+					System.out.println("Entre o dia do mês !!! ");
+					dia = scan.nextInt();
+					if (dia > 0 & dia <= 31) {
+						diaValido = true;
+					} else {
+						System.out.println("Entre com um dia valido");
+					}
+				}
+				while (!hrValida) {
+					System.out.println("Informe a hora do compromisso !!! ");
+					hora = scan.nextInt();
+					if (hora >= 0 & hora <= 24) {
+						hrValida = true;
+					} else {
+						System.out.println("Hora invalida, digite novamente");
+					}
+				}
+			}
+				// Verificar o compromisso
+
+				System.out.println("Digite o compromisso ");
+				compromissos[dia][hora] = scan.next();
+			case 2: {
+
+				while (!diaValido) {
+					System.out.println("Entre o dia do mês !!! ");
+					dia = scan.nextInt();
+					if (dia > 0 & dia <= 31) {
+						diaValido = true;
+					} else {
+						System.out.println("Entre com um dia valido");
+					}
+				}
+				while (!hrValida) {
+					System.out.println("Informe a hora do compromisso !!! ");
+					hora = scan.nextInt();
+					if (hora >= 0 & hora <= 24) {
+						hrValida = true;
+					} else {
+						System.out.println("Hora invalida, digite novamente");
+					}
+				}
+
+				System.out.println("Compromissos agendado é ");
+				System.out.println(compromissos[dia][hora]);
+				break;
+			}
+			case 0: {
+				sair = true;
+			}
+				System.out.println("Opção Invalida digite novamente !!!");
+			}
+		}
 	}
 
 }
