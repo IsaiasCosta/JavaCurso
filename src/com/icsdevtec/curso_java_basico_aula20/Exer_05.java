57e01f222e07e5959ff38ad95995e5047edc2031
icsdevtec <icsdevtec@DESKTOP-U0729KU>
2023-05-23 22:44:07 -0300

    commit jogo da velha e agenda

--- src/com/icsdevtec/curso_java_basico_aula20/Exer_05.java
+++ src/com/icsdevtec/curso_java_basico_aula20/Exer_05.java
@@ -3,6 +3,8 @@
  */
 package com.icsdevtec.curso_java_basico_aula20;
 
+import java.util.Scanner;
+
 /**
  * @author icsdevtec
  *
@@ -13,7 +15,126 @@ public class Exer_05 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+
+		String[][][] compromissos = new String[12][31][9];
+
+		boolean mesValido = false;
+
+		boolean sair = false;
+		boolean diaValido = false;
+
+		boolean hrValida = false;
+		int hora = 0;
+		int dia = 0;
+		int mes = 0;
+		byte opcao;
+		while (!sair) {
+			System.out.println(" Digite 1 para adicionar o compromisso !!! ");
+			System.out.println(" Digite 2 para verificar o compromisso !!! ");
+			System.out.println(" Digite 0 para sair do Programa !!! ");
+			opcao = scan.nextByte();
+
+			switch (opcao) {
+
+			// Adicionar o compromisso
+			case 1: {
+
+				while (!mesValido) {
+					System.out.println("Entre o  mês do ano !!! ");
+					mes = scan.nextInt();
+					if (mes > 0 & dia <= 12) {
+						mesValido = true;
+					} else {
+						System.out.println("Entre com um mês valido");
+					}
+				}
+				while (!diaValido) {
+					System.out.println("Entre o dia do mês !!! ");
+					dia = scan.nextInt();
+					if (dia > 0 & dia <= 31) {
+						diaValido = true;
+					} else {
+						System.out.println("Entre com um dia valido");
+					}
+				}
+				while (!hrValida) {
+					System.out.println("Informe a hora do compromisso !!! ");
+					hora = scan.nextInt();
+					if (hora >= 0 & hora <= 8) {
+						hrValida = true;
+					} else {
+						System.out.println("Hora invalida, digite novamente");
+					}
+				}
+			}
+			case 2: {
+				while (!mesValido) {
+					System.out.println("Entre o  mês do ano !!! ");
+					mes = scan.nextInt();
+					if (mes > 0 & dia <= 12) {
+						mesValido = true;
+					} else {
+						System.out.println("Entre com um mês valido");
+					}
+				}
+				while (!diaValido) {
+					System.out.println("Entre o dia do mês !!! ");
+					dia = scan.nextInt();
+					if (dia > 0 & dia <= 31) {
+						diaValido = true;
+					} else {
+						System.out.println("Entre com um dia valido");
+					}
+				}
+				while (!hrValida) {
+					System.out.println("Informe a hora do compromisso !!! ");
+					hora = scan.nextInt();
+					if (hora >= 0 & hora <=8) {
+						hrValida = true;
+					} else {
+						System.out.println("Hora invalida, digite novamente");
+					}
+				}
+			}
+				// Verificar o compromisso
+				
+			
+			
+				System.out.println("Digite o compromisso ");
+				compromissos[mes][dia][hora] = scan.next();
+			case 3: {
+
+				while (!diaValido) {
+					System.out.println("Entre o dia do mês !!! ");
+					dia = scan.nextInt();
+					if (dia > 0 & dia <= 31) {
+						diaValido = true;
+					} else {
+						System.out.println("Entre com um dia valido");
+					}
+				}
+				while (!hrValida) {
+					System.out.println("Informe a hora do compromisso !!! ");
+					hora = scan.nextInt();
+					if (hora >= 0 & hora <= 8) {
+						hrValida = true;
+					} else {
+						System.out.println("Hora invalida, digite novamente");
+					}
+				}
+			
+				
+				System.out.println("Compromissos agendado é ");
+				System.out.println(compromissos[mes][dia][hora]);
+				
+			}
+			case 0: {
+				sair = true;
+			}
+				System.out.println("Opção Invalida digite novamente !!!");
+			}
+		}
 
 	}
 
--- src/com/icsdevtec/curso_java_basico_aula20/Exer_06.java
+++ src/com/icsdevtec/curso_java_basico_aula20/Exer_06.java
@@ -3,6 +3,8 @@
  */
 package com.icsdevtec.curso_java_basico_aula20;
 
+import java.util.Scanner;
+
 /**
  * @author icsdevtec
  *
@@ -13,7 +15,94 @@ public class Exer_06 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		char[][] jogoVelha = new char[3][3];
+
+		System.out.println("Jogador nº 1  = X ");
+		System.out.println("Jogador nº 2  = O ");
+
+		boolean ganhou = false;
+		int jogador = 1;
+		char sinal;
+		int linha = 0, coluna = 0;
+		// informado de quem é a vez de jogar
+		while (!ganhou) {
+			if (jogador % 2 == 1) {
+				System.out.println("Jogador nº 1 sua vez de Jogar ");
+				sinal = 'X';
+			} else {
+				System.out.println("Jogador nº 2 sua vez de Jogar");
+				sinal = 'O';
+			}
+			// validando a linha
+
+			boolean linhaValida = false;
+			while (!linhaValida) {
+				System.out.println("Entre com uma linha valida [1, 2 ou 3] ");
+				linha = scan.nextInt();
+				if (linha >= 1 && linha <= 3) {
+					linhaValida = true;
+
+				} else {
+					System.out.println("Linha invalida, a linha deve ser 1,2 ou 3");
+				}
+			}
+			// validando a coluna
+
+			boolean colunaValida = false;
+			while (!colunaValida) {
+				System.out.println("Entre com uma coluna valida [1, 2 ou 3] ");
+				coluna = scan.nextInt();
+				if (coluna >= 1 && coluna <= 3) {
+					colunaValida = true;
+
+				} else {
+					System.out.println("Coluna invalida, a Coluna deve ser 1,2 ou 3");
+				}
+			}
+			// controlando o tabuleiro
+			linha--;
+			coluna--;
+			if (jogoVelha[linha][coluna] == 'X' || jogoVelha[linha][coluna] == 'O') {
+				System.out.println(" A Posição escolhida, já esta oculpada, tente novamente ");
+			} else {
+				jogoVelha[linha][coluna] = sinal;
+				jogador++;
+			}
+			// Imprimindo o Tabuleiro
+
+			for (int i = 0; i < jogoVelha.length; i++) {
+				for (int j = 0; j < jogoVelha[i].length; j++) {
+					System.out.print(jogoVelha[i][j] + " | ");
+				}
+				System.out.println();
+			}
+			// Quem ganhou ou se houve empate
+			if ((jogoVelha[0][0] == 'X' && jogoVelha[0][1] == 'X' && jogoVelha[0][2] == 'X') || // linha 1
+					(jogoVelha[1][0] == 'X' && jogoVelha[1][1] == 'X' && jogoVelha[1][2] == 'X') || // linha 2
+					(jogoVelha[2][0] == 'X' && jogoVelha[2][1] == 'X' && jogoVelha[2][2] == 'X') || // linha 3
+					(jogoVelha[0][0] == 'X' && jogoVelha[1][0] == 'X' && jogoVelha[2][0] == 'X') || // coluna 1
+					(jogoVelha[0][1] == 'X' && jogoVelha[1][1] == 'X' && jogoVelha[2][1] == 'X') || // coluna 2
+					(jogoVelha[0][2] == 'X' && jogoVelha[1][2] == 'X' && jogoVelha[2][2] == 'X') || // coluna 3
+					(jogoVelha[0][0] == 'X' && jogoVelha[1][1] == 'X' && jogoVelha[2][2] == 'X')) {
+				ganhou = true;
+				System.out.println("Jogador nº 1 ganhou !!!");
+			} else {
+				if ((jogoVelha[0][0] == 'O' && jogoVelha[0][1] == 'O' && jogoVelha[0][2] == 'O') || // linha 1
+						(jogoVelha[1][0] == 'O' && jogoVelha[1][1] == 'O' && jogoVelha[1][2] == 'O') || // linha 2
+						(jogoVelha[2][0] == 'O' && jogoVelha[2][1] == 'O' && jogoVelha[2][2] == 'O') || // linha 3
+						(jogoVelha[0][0] == 'O' && jogoVelha[1][0] == 'O' && jogoVelha[2][0] == 'O') || // coluna 1
+						(jogoVelha[0][1] == 'O' && jogoVelha[1][1] == 'O' && jogoVelha[2][1] == 'O') || // coluna 2
+						(jogoVelha[0][2] == 'O' && jogoVelha[1][2] == 'O' && jogoVelha[2][2] == 'O') || // coluna 3
+						(jogoVelha[0][0] == 'O' && jogoVelha[1][1] == 'O' && jogoVelha[2][2] == 'O')) {
+					ganhou = true;
+					System.out.println("Jogador nº 2 ganhou !!!");
+				}else if (jogador >9) {
+					ganhou=true;
+					System.out.println("Não houve ganhador, nesta jogada !!!");
+				}
+			}
+		}
 
 	}
 
