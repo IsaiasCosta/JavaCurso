77478f8e786acb7cf433241defad43bac08e4389
icsdevtec <icsdevtec@DESKTOP-U0729KU>
2023-05-26 22:37:39 -0300

    commit classes

---
+++ src/com/icsdevtec/curso_java_basico_aula24/Carro.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.icsdevtec.curso_java_basico_aula24;
+
+/**
+ * @author icsdevtec
+ *
+ */
+public class Carro { 
+	
+	//Atributos
+
+	String marca;
+	String modelo;
+	int qdtPassageiros;
+	double tanqueCombustivel;
+	double ConsCombustivel;
+
+}
---
+++ src/com/icsdevtec/curso_java_basico_aula24/TiposCarro.java
@@ -0,0 +1,16 @@
+package com.icsdevtec.curso_java_basico_aula24;
+
+public class TiposCarro {
+
+	public static void main(String[] args) {
+
+		 Carro corolla= new Carro();	 
+		 corolla.marca="toiota";
+		 corolla.modelo="cross";
+		 corolla.qdtPassageiros=5;
+		 corolla.tanqueCombustivel=36;
+		 corolla.ConsCombustivel =11.8;
+
+	}
+
+}
